d6870da732d3c1d776f12242e77b7d538a168445
James David Thorley <101204516+James220903@users.noreply.github.com>
2023-12-13 11:42:49 +0000

    Update UnitTest1.cs
    removed test

--- Staff Monitor Engagement UnitTest/UnitTest1.cs
+++ Staff Monitor Engagement UnitTest/UnitTest1.cs
@@ -17,47 +17,5 @@ namespace Staff_Monitor_Engagement_UnitTest
         }
     }
     
-    [TestClass]
-    public class LoginTests
-    {
-        private Mock<Database> mockDatabase;
 
-        [SetUp]
-        public void Setup()
-        {
-            mockDatabase = new Mock<Database>();
-        }
-
-        [TestMethod]
-        public void Login_ValidStudentCredentials_ReturnsStudentRole()
-        {
-            // Arrange
-            string validStudentUsername = "studentUser";
-            string validStudentPassword = "studentPass";
-            mockDatabase.Setup(db => db.IsUserValid(validStudentUsername, validStudentPassword, "STUDENT")).Returns(true);
-
-            // Act
-            var result = mockDatabase.Object.Login(validStudentUsername, validStudentPassword);
-
-            // Assert
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(UserRole.Student, result);
-        }
-
-        [TestMethod]
-        public void Login_InvalidCredentials_ReturnsNoneRole()
-        {
-            // Arrange
-            string invalidUsername = "invalidUser";
-            string invalidPassword = "invalidPass";
-            mockDatabase.Setup(db => db.IsUserValid(invalidUsername, invalidPassword, It.IsAny<string>())).Returns(false);
-
-            // Act
-            var result = mockDatabase.Object.Login(invalidUsername, invalidPassword);
-
-            // Assert
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(UserRole.None, result);
-        }
-
-        // Additional tests for PersonalSupervisor and SeniorTutor roles
-    }
 }
