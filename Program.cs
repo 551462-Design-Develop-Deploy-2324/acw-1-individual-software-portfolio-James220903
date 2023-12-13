32e852a3d84d78bd9c46b58efd66d934d24e6ab8
james thorley <jamesthorley291@gmail.com>
2023-12-13 11:49:44 +0000

    added using System.Runtime.CompilerServices;

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
 using System.Security.Cryptography.X509Certificates;
+using System.Runtime.CompilerServices;
 
 
 namespace Staff_Monitor_Engagement
